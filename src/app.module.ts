6aa6cc5beaff0c8977a63ebf3ad508c467f57f62
EyaAbbassi <eya.abbassi@yahoo.com>
2025-02-12 18:55:09 +0100

    feat(docker): add build dovker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EvaluationModule } from './modules/evaluation/evaluation.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}), // Load .env variables
-    EvaluationModule
+    ConfigModule.forRoot({isGlobal: true}),
+    EvaluationModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
